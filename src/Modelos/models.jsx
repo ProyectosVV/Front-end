9e5f6e69c5344fee489b0c209b45f666a093ae40
LeninVasq <proyectoslv97@gmail.com>
2025-09-24 09:47:59 -0600

    Modelos nuevos

--- src/Modelos/models.jsx
+++ src/Modelos/models.jsx
@@ -1,11 +1,61 @@
+export class Pais {
+  constructor(id, nombrePais, estado = 1) {
+    this.id = id;
+    this.nombrePais = nombrePais;
+    this.estado = estado;
+  }
+
+  static registrar(nombrePais, estado = 1) {
+    const paises = JSON.parse(sessionStorage.getItem("paises")) || [];
+
+    const nuevoPais = new Pais(paises.length + 1, nombrePais, estado);
+
+    paises.push(nuevoPais);
+
+    sessionStorage.setItem("paises", JSON.stringify(paises));
+
+    return nuevoPais;
+  }
+
+  static obtenerTodos() {
+    return JSON.parse(sessionStorage.getItem("paises")) || [];
+  }
+}
+
+export class Ciudad {
+  constructor(id, nombreCiudad, paisId, estado = 1) {
+    this.id = id;
+    this.nombreCiudad = nombreCiudad;
+    this.paisId = paisId;  // Relación: FK al ID del país
+    this.estado = estado;
+  }
+
+  static registrar(nombreCiudad, paisId, estado = 1) {
+    const ciudades = JSON.parse(sessionStorage.getItem("ciudades")) || [];
+
+    const nuevaCiudad = new Ciudad(ciudades.length + 1, nombreCiudad, paisId, estado);
+
+    ciudades.push(nuevaCiudad);
+
+    sessionStorage.setItem("ciudades", JSON.stringify(ciudades));
+
+    return nuevaCiudad;
+  }
+
+  static obtenerTodos() {
+    return JSON.parse(sessionStorage.getItem("ciudades")) || [];
+  }
+}
+
+
 export class Rol {
-  constructor(id, nombreRol, estado = "activo") {
+  constructor(id, nombreRol, estado = 1) {
     this.id = id;             
     this.nombreRol = nombreRol; 
     this.estado = estado;       
   }
 
-  static registrar(nombreRol, estado = "activo") {
+  static registrar(nombreRol, estado = 1) {
     const roles = JSON.parse(sessionStorage.getItem("roles")) || [];
 
     const nuevoRol = new Rol(roles.length + 1, nombreRol, estado);
@@ -19,18 +69,21 @@ export class Rol {
 }
 
 
+
 export class Usuario {
-  constructor(id, nombre, correo, contrasena, rol) {
+  constructor(id, nombre, correo, contrasena, rol,telefono,estado= 1 ) {
     this.id = id;
     this.nombre = nombre;
     this.correo = correo;
     this.contrasena = contrasena;
     this.rol = rol;
+    this.telefono = telefono;
+    this.estado = estado;
   }
 
-   static registrar(nombre, correo, contrasena, rol) {
+   static registrar(nombre, correo, contrasena, rol,telefono, estado = 1) {
     const usuarios = JSON.parse(sessionStorage.getItem("usuarios")) || [];
-    const nuevo = new Usuario(usuarios.length + 1, nombre, correo, contrasena, rol);
+    const nuevo = new Usuario(usuarios.length + 1, nombre, correo, contrasena, rol, telefono,estado);
     usuarios.push(nuevo);
     sessionStorage.setItem("usuarios", JSON.stringify(usuarios));
     return nuevo;
--- src/dataInit.js
+++ src/dataInit.js
@@ -8,13 +8,12 @@ const rolPasajero = new Rol(2, "pasajero");
 const rolesDemo = [rolAdmin, rolPasajero];
 
 const usuariosDemo = [
-  new Usuario(1, "Admin", "admin@avianca.com", "1234", 1),
-  new Usuario(2, "Juan Pérez", "juan@gmail.com", "abcd", 2),
-  new Usuario(3, "Maria Lopez", "maria@gmail.com", "abcd", 2),
+  new Usuario(1, "Admin", "admin@avianca.com", "1234", 1, "7789-8987"),
+  new Usuario(2, "Juan Pérez", "juan@gmail.com", "abcd", 2, "7868-8942"),
+  new Usuario(3, "Maria Lopez", "maria@gmail.com", "abcd", 2, "8798-4875"),
 ];
 
 export function initUsuarios() {
-  // Inicializar datos
   const rolesExistentes = JSON.parse(sessionStorage.getItem("roles"));
   if (!rolesExistentes) {
     sessionStorage.setItem("roles", JSON.stringify(rolesDemo));
